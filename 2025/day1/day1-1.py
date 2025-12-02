09640cf9b84dd7eb690fb00bb67cb54aac73a0c4
Eric Thomas <ethomasosu@gmail.com>
2025-12-01 22:41:01 -0500

    optimize day 1 part 1

--- 2025/day1/day1-1.py
+++ 2025/day1/day1-1.py
@@ -2,20 +2,17 @@ with open("inputs/day1-data.txt", "r") as fp:
     rotations = [line.strip() for line in fp.readlines()]
 
 current_number = 50
-points_at_0_count = 0
+password = 0
 
 for rotation in rotations:
-    if rotation[0] == "R":
-        for i in range(int(rotation[1:])):
-            current_number += 1
-            if current_number == 100:
-                current_number = 0
+    direction = rotation[0]
+    amount = int(rotation[1:])
+    if direction == "R":
+        current_number = (current_number + amount) % 100
     else:
-        for i in range(int(rotation[1:])):
-            current_number -= 1
-            if current_number == -1:
-                current_number = 99
-    if current_number == 0:
-        points_at_0_count += 1
+        current_number = (current_number - amount) % 100
 
-print(f"password: {points_at_0_count}")
+    if current_number == 0:
+        password += 1
+
+print(f"password: {password}")
